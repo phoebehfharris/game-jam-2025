e495497dfb626e105410a26ff58bcbffb78ebccd
Phoebe Harris <phoebe.harris.2024@proton.me>
2025-11-15 18:45:05 +0000

    Improved rendering and implemented grid navigation

--- Scripts/Character.cs
+++ Scripts/Character.cs
@@ -5,13 +5,29 @@ public partial class Character : CharacterBody2D
 {
 
 	TileMapLayer tile_map;
-	Camera2D camera;
+	NavigationAgent2D nav;
 	
 
 	public override void _Ready()
 	{
 		tile_map = GetNode<TileMapLayer>("../TileMapLayer");
-		camera = GetNode<Camera2D>("../Camera2D");
+		nav = GetNode<NavigationAgent2D>("NavigationAgent2D");
+		nav.VelocityComputed += OnNavigationAgent2DVelocityComputed;
+	}
+	
+	public override void _PhysicsProcess(double delta) {
+		if (nav.IsNavigationFinished()) {return;}
+
+		GD.Print("Meow");
+		Vector2 next_position = nav.GetNextPathPosition();
+		
+		GD.Print(nav.TargetPosition);
+		GD.Print(next_position);
+		GD.Print(this.GlobalPosition);
+		
+		Vector2 velocity = this.GlobalPosition.DirectionTo(next_position) * 250.0f;
+		
+		nav.Velocity = velocity;
 	}
 
 	public override void _Input(InputEvent @event)
@@ -19,8 +35,16 @@ public partial class Character : CharacterBody2D
 		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed && mouseEvent.ButtonIndex == MouseButton.Left) 
 		{
 			Vector2 global_space = GetGlobalMousePosition();
-			Vector2I	 map_coords = tile_map.LocalToMap(global_space);
-			GD.Print(map_coords);
+			Vector2 local_space = tile_map.ToLocal(global_space);
+			Vector2I	 map_coords = tile_map.LocalToMap(local_space);
+			Vector2 map_position = tile_map.MapToLocal(map_coords);
+			nav.TargetPosition = map_position; 
 		}
+	}
+	
+	public void OnNavigationAgent2DVelocityComputed(Vector2 safe_velocity) {
+		GD.Print("Buhhh");
+		this.Velocity = safe_velocity;
+		MoveAndSlide();
 	}
 }
